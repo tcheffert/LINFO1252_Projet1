5bfd86f81d97602eef89201d0bb8202dc9bb83db
tcheffert <theo.cheffert@student.uclouvain.be>
2024-11-14 18:16:44 +0100

    Update 1.3 (boucle for pour write & read)

--- readers_writers.c
+++ readers_writers.c
@@ -1,6 +1,10 @@
 #include <semaphore.h>
 #include <pthread.h>
 
+// Nombre de fois où les lecteurs et écrivains s'effecturont
+#define N_readings 2540
+#define N_writings 640
+
 // Initialisation des mutex et des semaphores
 pthread_mutex_t mutex_reader;
 pthread_mutex_t mutex_writer;
@@ -10,68 +14,73 @@ sem_t db_reader;
 sem_t db_writer;
 
 int writercount = 0;
-int readcount=0; // nombre de readers
+int readcount = 0; // nombre de readers
 
-/// @brief Fonction simulant un traitement
+// Fonction simulant un traitement
 void process()
 {
-    for (int i = 0; i < 10000; i++);
+    for (int i = 0; i < 10000; i++)
+        ;
 }
 
-/// @brief Fonction de lecture de l'algorithme des lecteurs-écrivains
-void* reader()
+// Fonction Reader
+void *reader()
 {
-    while (1)
+    // Le(s) lecteur(s) effectue(nt) 2540 lectures
+    for (int i = 0; i < N_readings; i++)
     {
-        pthread_mutex_lock(&z); //? Permet de donner la priorité absolue aux writers
 
-        sem_wait(&db_reader); //? Sémaphore permettant aux écrivains de bloquer les lecteurs
- 
+        pthread_mutex_lock(&z); // Permet de donner la priorité absolue aux writers
+
+        sem_wait(&db_reader); // Sémaphore permettant aux écrivains de bloquer les lecteurs
+
         pthread_mutex_lock(&mutex_reader); //? Mutex pour protéger la variable readcount
         readcount++;
 
         if (readcount == 1)
-            sem_wait(&db_writer); //? premier reader réserve la database pour être sûr qu'aucun writer ne l'utilise
+            sem_wait(&db_writer); // Premier reader réserve la database pour être sûr qu'aucun writer ne l'utilise
 
         pthread_mutex_unlock(&mutex_reader);
         sem_post(&db_reader);
         pthread_mutex_unlock(&z);
-        
-        //! read_database();
-        //? On peut lire tranquille car les writers sont bloqués par le sémaphore db_writer
+
+        //===== Section critique =====//
+        process(); // On peut process car les writers sont bloqués par le sémaphore db_writer
 
         pthread_mutex_lock(&mutex_reader);
         readcount--;
-        if(readcount==0)
+        if (readcount == 0)
             sem_post(&db_writer); // dernier reader libère la database écrivain
 
         pthread_mutex_unlock(&mutex_reader);
     }
 }
 
-/// @brief Fonction d'écriture de l'algorithme des lecteurs-écrivains
-void* writer()
+// Fonction Writer
+void *writer()
 {
-    while(1) {
-        pthread_mutex_lock(&mutex_writer); //? Mutex pour protéger la variable writercount
+    // Le(s) écrivain(s) effectue(nt) 640 écritures
+    for (int i = 0; i < N_writings; i++)
+    {
+
+        pthread_mutex_lock(&mutex_writer); // Mutex pour protéger la variable writercount
         writercount++;
-        if (writercount==1) // arrivée du premier writer
+        if (writercount == 1)     // arrivée du premier writer
             sem_wait(&db_reader); // bloque les lecteurs
         pthread_mutex_unlock(&mutex_writer);
 
-        sem_wait(&db_writer); // bloque les autres writers et vérifie que la database 
+        sem_wait(&db_writer); // bloque les autres writers et vérifie que la database
                               // n'est pas en train d'être lue
 
-        //! section critique, un seul writer à la fois
-        //! write_database();
-        //? On peut écrire tranquille car les lecteurs sont bloqués par le sémaphore db_reader, 
-        //? et les autres writers par db_writer
+        //===== Section critique =====//
+        // Un seul writer à la fois. On peut write car les lecteurs sont bloqués par le sémaphore db_reader et les autres writers par db_writer
+        process();
 
         sem_post(&db_writer); // libère les autres writers
 
         pthread_mutex_lock(&mutex_writer);
         writercount--;
-        if(writercount==0) // départ du dernier writer
+        if (writercount == 0)     // départ du dernier writer
             sem_post(&db_reader); // libère les lecteurs
         pthread_mutex_unlock(&mutex_writer);
     }
