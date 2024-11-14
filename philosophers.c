b8b7669de2876176bf1fe5204adc43b5606acd02
tcheffert <theo.cheffert@student.uclouvain.be>
2024-11-14 19:12:50 +0100

    com philo

--- philosophers.c
+++ philosophers.c
@@ -3,7 +3,7 @@
 #define cycles 1000000  // Nombre de cycles penser/manger pour chaque philospohe
 
 int N;  // Nombre de philospohes (sera donné par argument plus tard)
-pthread_mutex_t *fourchettes;  // Tableau représentant les fourchettes
+// Faire un tableau représentant les fourchettes
 
 
 void* philosophe(void* id) {
