563a474a73bce5cbc5c7f8c6d50c00e02d935d6d
Bibinado <arnaud.belliere@student.uclouvain.be>
2024-11-16 15:31:54 +0100

    start philosophe funct

--- philosophers.c
+++ philosophers.c
@@ -6,6 +6,25 @@ int N;  // Nombre de philospohes (sera donné par argument plus tard)
 // Faire un tableau représentant les fourchettes
 
 
-void* philosophe(void* id) {
+sem_t* forks; // Tableau de sémaphores pour les fourchettes
+
+void* philosophe(void* arg) {
+    int *id = (int *) arg;
+    int left = *id;
+    int right = (*id + 1) % N;
+
+    int count = 0;
+    while ( count < cycles){
+        if (left < right){ // penser
+            lock(&forks[left]);
+            lock(&forks[right]);
+        } else {
+            lock(&forks[right]);
+            lock(&forks[left]);
+        }
+        unlock(&forks[left]); // manger
+        unlock(&forks[right]);
+        count++;
+    }
     return (NULL);   
 }
