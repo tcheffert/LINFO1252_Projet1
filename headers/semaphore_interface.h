de55401d5b75291615b62273f9823bfab09aa0d7
tcheffert <theo.cheffert@student.uclouvain.be>
2024-11-30 16:05:27 +0100

    Modif semaphore interface: check argument function

--- headers/semaphore_interface.h
+++ headers/semaphore_interface.h
@@ -10,15 +10,27 @@ typedef struct
     pthread_mutex_t lock;     // Mutex pour protéger les sections critiques
 } semaphore_t;
 
+int checkArg(semaphore_t *sem, const char *func_name)
+{
+    if (sem == NULL)
+    {
+        fprintf(stderr, "Erreur dans %s : pointeur de sémaphore NULL !\n", func_name);
+        return -1; // Erreur pointeur semaphore: return -1
+    }
+    return 0; // Pas d'erreurs: return 0
+}
+
 // Initialisation du sémaphore
 void semaphore_init(semaphore_t *sem, int initial_value)
 {
-    sem->value = initial_value;               // Initialisation de la valeur du sémaphore
-    pthread_mutex_init(&sem->lock, NULL);     // Initialisation du mutex
-    pthread_cond_init(&sem->condition, NULL); // Initialisation de la variable de condition
+    if (checkArg(sem, "semaphore_init") == -1)
+        return; // Erreur argument
+
+    sem->value = initial_value; // Initialisation de la valeur du sémaphore
+    // Initialisation du mutex et de la variable de condition
     if (pthread_mutex_init(&sem->lock, NULL) != 0 || pthread_cond_init(&sem->condition, NULL) != 0)
     {
-        perror("Erreur d'initialisation du mutex (void semaphore_init)");
+        perror("Erreur d'initialisation (void semaphore_init)");
         exit(1);
     }
 }
@@ -26,13 +38,23 @@ void semaphore_init(semaphore_t *sem, int initial_value)
 // Destruction du sémaphore
 void semaphore_destroy(semaphore_t *sem)
 {
-    pthread_mutex_destroy(&sem->lock);     // Destruction du mutex
-    pthread_cond_destroy(&sem->condition); // Destruction de la variable de condition
+    if (checkArg(sem, "semaphore_destroy") == -1)
+        return; // Erreur argument
+
+    // Destruction du mutex et de la variable de condition si elles ont été correctement initialisées
+    if (pthread_mutex_destroy(&sem->lock) != 0 || pthread_cond_destroy(&sem->condition) != 0)
+    {
+        perror("Erreur lors destruction (void semaphore_destroy)");
+        return;
+    }
 }
 
 // Fonction Wait (P) avec attente active
 void semaphore_wait(semaphore_t *sem)
 {
+    if (checkArg(sem, "semaphore_wait") == -1)
+        return; // Erreur argument
+
     pthread_mutex_lock(&sem->lock); // Entrée dans la section critique
 
     while (sem->value <= 0)                             // Tant que la ressource n'est pas disponible
@@ -48,6 +70,9 @@ void semaphore_wait(semaphore_t *sem)
 // Fonction Signal (V)
 void semaphore_signal(semaphore_t *sem)
 {
+    if (checkArg(sem, "semaphore_signal") == -1)
+        return; // Erreur argument
+
     pthread_mutex_lock(&sem->lock); // Entrée dans la section critique
 
     sem->value++;                         // Augmente le nombre de ressources disponibles
