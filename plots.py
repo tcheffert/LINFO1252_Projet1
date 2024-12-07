1bcd9de4f92e1e64db6e950bacf5c6960aedecb0
tcheffert <theo.cheffert@student.uclouvain.be>
2024-12-07 22:00:08 +0100

    Maj plot: mise à l'échelle

--- plots.py
+++ plots.py
@@ -49,7 +49,7 @@ test_and_test_and_set_data_studsrv = pd.read_csv(test_and_test_and_set_csv_studs
 
 
 
-def plot_performance(data, title, output_file, color="blue", saveFig=False):
+def plot_performance(data, yMax, title, output_file, color="blue", saveFig=False):
     """
     Plots the execution time vs. number of threads with mean and std deviation.
     
@@ -68,10 +68,10 @@ def plot_performance(data, title, output_file, color="blue", saveFig=False):
     plt.errorbar(means.index, means, yerr=stds, fmt='-o', color=color, capsize=7, label="Execution time with standard deviation")
     
     # Graph settings
-    plt.title(title)
+    plt.title(title, weight='bold')
     plt.xlabel("Number of Threads")
     plt.ylabel("Execution Time [s]")
-    plt.ylim(0)  # y-axis starts at 0
+    plt.ylim(0, yMax)  # y-axis starts at 0 and go to yMax
     plt.xticks(means.index)  # Ensure x-axis matches thread counts
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.legend()
@@ -81,26 +81,26 @@ def plot_performance(data, title, output_file, color="blue", saveFig=False):
         plt.savefig('plots/' + output_file + ".pdf", format='pdf', bbox_inches="tight")
     plt.show()
 
-save = False
+save = True
 
 #===Plot for Producers/Consumers===#
-plot_performance(pc_data, "Performance Producers/Consumers POSIX depending on the numbers of Threads", "pc_POSIX_local", color="cornflowerblue", saveFig=save)
-plot_performance(pc_data_TAS, "Performance Producers/Consumers TAS depending on the numbers of Threads", "pc_TAS_local", color="slateblue", saveFig=save)
-plot_performance(pc_data_TATAS, "Performance Producers/Consumers TATAS depending on the numbers of Threads", "pc_TATAS_local", color="cornflowerblue", saveFig=save)
+plot_performance(pc_data, 2.25, "Performance Producers/Consumers POSIX depending on the numbers of Threads", "pc_POSIX_scaled", color="cornflowerblue", saveFig=save)
+plot_performance(pc_data_TAS, 2.25, "Performance Producers/Consumers TAS depending on the numbers of Threads", "pc_TAS_scaled", color="slateblue", saveFig=save)
+plot_performance(pc_data_TATAS, 2.25, "Performance Producers/Consumers TATAS depending on the numbers of Threads", "pc_TATAS_scaled", color="cornflowerblue", saveFig=save)
 
 #===Plot for Readers/Writers===#
-plot_performance(rw_data, "Performance Readers/Writers POSIX depending on the numbers of Threads", "rw_POSIX_local", color="mediumseagreen", saveFig=save)
-plot_performance(rw_data_TAS, "Performance Readers/Writers TAS depending on the numbers of Threads", "rw_TAS_local", color="mediumaquamarine", saveFig=save)
-plot_performance(rw_data_TATAS, "Performance Readers/Writers TATAS depending on the numbers of Threads", "rw_TATAS_local", color="forestgreen", saveFig=save)
+plot_performance(rw_data, 0.6,  "Performance Readers/Writers POSIX depending on the numbers of Threads", "rw_POSIX_scaled", color="mediumseagreen", saveFig=save)
+plot_performance(rw_data_TAS, 0.6, "Performance Readers/Writers TAS depending on the numbers of Threads", "rw_TAS_scaled", color="mediumaquamarine", saveFig=save)
+plot_performance(rw_data_TATAS, 0.6, "Performance Readers/Writers TATAS depending on the numbers of Threads", "rw_TATAS_scaled", color="forestgreen", saveFig=save)
 
 #===Plot for Philosophers===#
-plot_performance(philo_data, "Performance Philosophers POSIX depending on the numbers of Threads", "philo_POSIX_local", color="crimson", saveFig=save)
-plot_performance(philo_data_TAS, "Performance Philosophers TAS depending on the numbers of Threads", "philo_TAS_local", color="red", saveFig=save)
-plot_performance(philo_data_TATAS, "Performance Philosophers TATAS depending on the numbers of Threads", "philo_TATAS_local", color="darkred", saveFig=save)
+plot_performance(philo_data, 8, "Performance Philosophers POSIX depending on the numbers of Threads", "philo_POSIX_scaled", color="crimson", saveFig=save)
+plot_performance(philo_data_TAS, 8, "Performance Philosophers TAS depending on the numbers of Threads", "philo_TAS_scaled", color="red", saveFig=save)
+plot_performance(philo_data_TATAS, 8, "Performance Philosophers TATAS depending on the numbers of Threads", "philo_TATAS_scaled", color="darkred", saveFig=save)
 
 #===Plot for Test and Set===#
-plot_performance(test_and_set_data, "Performance Test and Set depending on the numbers of Threads", "test_and_set_studsrv", color="darkorchid", saveFig=save)
+plot_performance(test_and_set_data, 0.8, "Performance Test and Set depending on the numbers of Threads", "test_and_set_scaled", color="darkorchid", saveFig=save)
 
 #===Plot for Test and Test and Set===#
-plot_performance(test_and_test_and_set_data_studsrv, "Performance Test and Test and Set depending on the numbers of Threads", "test_and_test_and_set_studsrv", color="darkorange", saveFig=save)
+plot_performance(test_and_test_and_set_data_studsrv, 0.8, "Performance Test and Test and Set depending on the numbers of Threads", "test_and_test_and_set_scaled", color="darkorange", saveFig=save)
 #plot_performance(test_and_test_and_set_data_30Runs, "Performance Test and Test and Set with 30 runs depending on the numbers of Threads", "test_and_test_and_set30Runs", color="darkgoldenrod", saveFig=save)
